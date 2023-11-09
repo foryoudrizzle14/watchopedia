e35dfc9174b9d6f8bee4cedef9944c55d44e457f
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-11-09 20:46:34 +0900

    adding counter and movie page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Counter from "./Counter";
 import Header from "./Header";
-import MoviePage from "./MovieComponenets/MoviePage";
+import MoviePage from "./MovieComponents/MoviePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
